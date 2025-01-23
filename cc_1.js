7138435531758037eeb6876a4c78a5891cecbdf2
Eugene Rodriguez <er11@usf.edu>
2025-01-23 14:05:44 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables //
+let employeeName = "Master Chief";
+const employeeID = 117;
+var isActive = true;
+
+console.log(employeeName);  // Master Chief
+console.log(typeof employeeName);  // string)
+console.log(employeeID);    // 117
+console.log(typeof employeeID);    // number
+console.log(isActive);      // true
+console.log( typeof isActive);      // boolean
