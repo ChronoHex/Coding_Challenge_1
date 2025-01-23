4b5a1fbe6349bea0d1aaa26b1831ee55ec88c46e
Eugene Rodriguez <er11@usf.edu>
2025-01-23 17:22:56 -0500

    Task 3 - Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -4,11 +4,11 @@ const employeeID = 117;
 var isActive = true;
 
 console.log(employeeName);  // Master Chief
-console.log(typeof employeeName);  // string)
+console.log(typeof employeeName);  // string
 console.log(employeeID);    // 117
 console.log(typeof employeeID);    // number
 console.log(isActive);      // true
-console.log( typeof isActive);      // boolean
+console.log(typeof isActive);      // boolean
 
 // Task 2: Primitive Data Types //
 let productName = "Halo 3";
@@ -20,4 +20,19 @@ console.log(typeof productName);  // string
 console.log(productPrice);    // 59.99
 console.log(typeof productPrice);    // number
 console.log(isAvailable);      // true
-console.log( typeof isAvailable);      // boolean
+console.log(typeof isAvailable);      // boolean
+
+// Task 3: Number Data Type //
+let accountBalance = 2000;
+
+accountBalance += 500;
+console.log(accountBalance);  // 2500
+
+accountBalance -= 500;
+console.log(accountBalance);  // 2000
+
+accountBalance *= 1.05;
+console.log(accountBalance);  // 2100
+
+accountBalance /= 2;
+console.log(accountBalance);  // 1050
