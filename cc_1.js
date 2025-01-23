b0c8dad05cbc4683ee620ac1bc4d534dfafeb516
Eugene Rodriguez <er11@usf.edu>
2025-01-23 14:17:42 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,15 @@ console.log(employeeID);    // 117
 console.log(typeof employeeID);    // number
 console.log(isActive);      // true
 console.log( typeof isActive);      // boolean
+
+// Task 2: Primitive Data Types //
+let productName = "Halo 3";
+const productPrice = 59.99;
+var isAvailable = true;
+
+console.log(productName);  // Halo 3
+console.log(typeof productName);  // string
+console.log(productPrice);    // 59.99
+console.log(typeof productPrice);    // number
+console.log(isAvailable);      // true
+console.log( typeof isAvailable);      // boolean
